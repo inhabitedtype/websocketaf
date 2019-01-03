58973ae00e6beb7e2f0e2891b353fdbab7794c6f
Andreas Garnaes <andreas.garnaes@gmail.com>
2019-01-03 21:39:56 +0100

    Update external interface

--- lib/websocketaf.ml
+++ lib/websocketaf.ml
@@ -1,2 +1,5 @@
+module Bigstring = Bigstring
 module Client_handshake = Client_handshake
-module Client_connetion = Client_connection
+module Client_connection = Client_connection
+module Wsd = Wsd
+module Websocket = Websocket
